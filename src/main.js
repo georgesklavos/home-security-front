df24d2f269bf4267f452337a8926e15be1a927f7
George <laos_2111@yahoo.gr>
2022-03-05 20:18:57 +0200

    add primeflex library and started the login page

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import { createApp } from 'vue'
 import "primevue/resources/themes/saga-blue/theme.css"
 import "primevue/resources/primevue.min.css"
-import "primeicons/primeicons.css"
+import "primeicons/primeicons.css";
+import  "/node_modules/primeflex/primeflex.css";
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import PrimeVue from "primevue/config";
 import Dialog from "primevue/dialog";
 import InputText from "primevue/inputtext";
+import Card from "primevue/card";
 
 const app = createApp(App)
 app.use(PrimeVue);
@@ -15,5 +17,6 @@ app.use(store)
 app.use(router)
 app.component("Dialog", Dialog);
 app.component("InputText",InputText);
+app.component("Card",Card);
 
 app.mount('#app')
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div class="flex justify-content-center align-items-center justify-content-center h-screen">
+  <Card style="width: 25rem; margin-bottom: 2em" class="flex  justify-content-center">
+            <template #content>
+                <div class="grid flex-column">
+                  <div class="col">
+                    
+                    <InputText type="text" placeholder="Email"></InputText>
+                  </div>
+                  <div class="col">
+                <InputText type="password" placeholder="Password"></InputText>
+
+                  </div>
+                </div>
+            </template>
+        </Card>
+</div>
 </template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: ""
+    }
+  }
+}
+</script>
+
+<style scoped>
+ #app {
+   height: 100vh;
+ }
+</style>
